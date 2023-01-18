cee8e8cefdd9f9fbeb9e9a3f6a375dba3b601c5f
douglasneves-net <d88.neves@gmail.com>
2023-01-18 12:56:01 -0300

    feat: add routes

--- 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/src/server.js
+++ 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/src/server.js
@@ -2,6 +2,17 @@ import http from 'node:http';
 
 
 const server = http.createServer((req, res) => {
+
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('List of users');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Create user');
+  }
+
   return res.end('Hello World');
 });
 
