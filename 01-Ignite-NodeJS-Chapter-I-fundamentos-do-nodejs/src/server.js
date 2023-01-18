05c7f81a00eea6d4609d98adc4303fdfd995ffba
douglasneves-net <d88.neves@gmail.com>
2023-01-18 15:32:20 -0300

    feat: return json

--- 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/src/server.js
+++ 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/src/server.js
@@ -1,15 +1,23 @@
 import http from 'node:http';
 
+const users = [];
 
 const server = http.createServer((req, res) => {
 
   const { method, url } = req;
 
   if (method === 'GET' && url === '/users') {
-    return res.end('List of users');
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Douglas',
+      email: 'd88.neves@gmail.com'
+    })
     return res.end('Create user');
   }
 
