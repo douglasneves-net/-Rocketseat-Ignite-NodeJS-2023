5ea80bba47ac1c0de22909063c4f069c368fdfc8
douglasneves-net <d88.neves@gmail.com>
2023-01-19 17:08:17 -0300

    feat: add full stream

--- 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/streams/fundamentals.js
+++ 01-Ignite-NodeJS-Chapter-I-fundamentos-do-nodejs/streams/fundamentals.js
@@ -2,4 +2,27 @@
 
 //Importação de clients via CSV (Excel)
 
-import { } from 'node:stream'
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+
+  index = 1;
+
+  // O Metodo _read é obrigatorio para leitura de streams
+  // Buffers não aceita numeros
+  _read() {
+
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
